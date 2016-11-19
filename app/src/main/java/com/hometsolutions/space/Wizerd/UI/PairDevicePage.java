a340bf5a5efa1d467857fab5fda8eff7f8fefdbb
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2016-11-19 10:22:23 +0530

    Add license: GNU GPL v3

--- app/src/main/java/com/hometsolutions/space/Wizerd/UI/PairDevicePage.java
+++ app/src/main/java/com/hometsolutions/space/Wizerd/UI/PairDevicePage.java
@@ -1,3 +1,22 @@
+
+/*
+ *
+ *                       Copyright (C) 2016 Bishwajyoti Roy
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.hometsolutions.space.Wizerd.UI;
 
 import android.support.v4.app.Fragment;
@@ -7,10 +26,6 @@ import com.tech.freak.wizardpager.model.Page;
 import com.tech.freak.wizardpager.model.ReviewItem;
 
 import java.util.ArrayList;
-
-/**
- * Created by Bishwajyoti Roy on 11/18/2016.
- */
 
 public class PairDevicePage extends Page {
 
